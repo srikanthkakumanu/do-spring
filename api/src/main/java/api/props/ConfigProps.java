35cb7c9dfd7fa3992e1f15b0bf1bfd840f9a80a5
Srikanth Kakumanu <srikanthkakumanu@yahoo.com>
2024-03-25 02:37:45 +0530

    Updated Spring boot api, todo, videos examples

---
+++ api/src/main/java/api/props/ConfigProps.java
@@ -0,0 +1,42 @@
+package api.props;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties(CustomProperties.class)
+public class ConfigProps {
+    public static void main(String[] args) {
+        var ctx = SpringApplication.run(ConfigProps.class, args);
+        var bean = ctx.getBean(CustomProperties.class);
+        System.out.println(bean.getFooter() + " " + bean.getHeader());
+    }
+
+}
+
+@Component
+@ConfigurationProperties(prefix="custom")
+class CustomProperties {
+    // if we need a default value, assign it here or the constructor
+    private String header, footer;
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+}
